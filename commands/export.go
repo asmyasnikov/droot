a1f37bf66b3dc54f6d9288e4e61cb425e622b8e5
amyasnikov <aleksey.myasnikov@bgoperator.com>
2020-06-17 12:25:20 +0300

    Add auto detect containerID/imageID. Replace creating .drootenv and .drootentry.sh as appending to tar pipe

--- commands/export.go
+++ commands/export.go
@@ -1,8 +1,13 @@
 package commands
 
 import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types/mount"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/urfave/cli"
@@ -23,40 +28,111 @@ var CommandExport = cli.Command{
 func doExport(c *cli.Context) error {
 	if len(c.Args()) < 1 {
 		cli.ShowCommandHelp(c, "export")
-		return errors.New("docker repository required")
+		return errors.New("docker imageID/containerID required")
 	}
-	repository := c.Args().Get(0)
-	if repository == "" {
+	id := c.Args().Get(0)
+	if id == "" {
 		cli.ShowCommandHelp(c, "export")
-		return errors.New("docker repository required")
+		return errors.New("docker imageID/containerID required")
 	}
-
 	docker, err := docker.New()
 	if err != nil {
 		return err
 	}
-	imageReader, err := docker.ExportImage(repository)
+	ctx := context.Background()
+	info, containerID, err := docker.Inspect(ctx, id)
+	defer func() {
+		if containerID != nil {
+			docker.Remove(ctx, *containerID)
+		}
+	}()
 	if err != nil {
 		return err
 	}
-	defer imageReader.Close()
-
+	reader, err := docker.Export(
+		ctx,
+		func() string {
+			if containerID != nil {
+				return *containerID
+			}
+			return info.ID
+		}(),
+		info,
+	)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	writer := os.Stdout
 	if output := c.String("output"); output != "" {
 		file, err := os.Create(output)
 		if err != nil {
 			return err
 		}
-		defer file.Close()
-
-		if _, err := io.Copy(file, imageReader); err != nil {
-			return errors.Wrapf(err, "Failed to write to the file %s", output)
-		}
-
-	} else {
-		if _, err := io.Copy(os.Stdout, imageReader); err != nil {
-			return errors.Wrapf(err, "Failed to write into stdout")
-		}
+		writer = file
 	}
-
+	defer func() {
+		writer.Close()
+	}()
+	if _, err := io.Copy(writer, reader); err != nil {
+		return err
+	}
+	cmd := "\tdroot run [--cp]"
+	if len(info.Config.User) > 0 {
+		cmd += " --user " + info.Config.User
+	}
+	for _, e := range info.Config.Env {
+		cmd += " --env " + e
+	}
+	attentions := ""
+	for _, m := range info.Mounts {
+		if m.Type != mount.TypeBind {
+			attentions += "\tmount point " + m.Source + ":" + m.Destination + " is a " + string(m.Type) + "\n"
+			continue
+		}
+		cmd += func() string {
+			if m.RW {
+				return " --bind "
+			}
+			return " --robind "
+		}() + m.Source + ":" + m.Destination
+	}
+	for p, b := range info.HostConfig.PortBindings {
+		attentions += "\tcontainer port binding " + p.Port() + " -> [ " + func() string {
+			return strings.Join(func() (bindings []string) {
+				for _, s := range b {
+					bindings = append(bindings, s.HostIP + ":" + s.HostPort)
+				}
+				return bindings
+			}(), ",")
+		}() + " ]\n"
+	}
+	for _, n := range info.NetworkSettings.Networks {
+		attentions += "\tcontainer have address " + n.IPAddress + " with network gateway " + n.Gateway + "\n"
+	}
+	if len(info.Config.WorkingDir) > 0 {
+		attentions += "\tcontainer have working directory " + info.Config.WorkingDir + "\n"
+	}
+	for _, l := range info.HostConfig.Ulimits {
+		attentions += "\tcontainer have ulimit " + l.String() + "\n"
+	}
+	if info.ContainerJSONBase.HostConfig.Resources.NanoCPUs > 0 {
+		attentions += "\tcontainer have limit cpus 0." + strconv.Itoa(int(info.ContainerJSONBase.HostConfig.Resources.NanoCPUs / 1000000)) + "\n"
+	}
+	if info.ContainerJSONBase.HostConfig.Resources.Memory > 0 {
+		attentions += "\tcontainer have limit memory " + strconv.Itoa(int(info.ContainerJSONBase.HostConfig.Resources.Memory / 1024 / 1024)) + "MB\n"
+	}
+	cmd += " --root [container directory]"
+	cmd += " -- " + func(s string) string {
+		if len(s) == 0 {
+			return s
+		}
+		return "[" + s + "] -c "
+	}(strings.Join(info.Config.Shell, "|")) + "\"" + strings.Join(append(info.Config.Entrypoint, info.Config.Cmd...), " ") + "\"\n"
+	fmt.Fprintln(os.Stderr, "Run droot with command (save this for future use):\n")
+	fmt.Fprintln(os.Stderr, cmd)
+	if len(attentions) > 0 {
+		fmt.Fprintln(os.Stderr, "Attentions:\n", attentions)
+	}
 	return nil
 }
--- docker/docker.go
+++ docker/docker.go
@@ -1,17 +1,16 @@
 package docker
 
 import (
-	"fmt"
-	"io"
-	"strings"
-
+	"bytes"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"github.com/pkg/errors"
-	"github.com/asmyasnikov/droot/environ"
 	"golang.org/x/net/context" // docker/docker don't use 'context' as standard package.
+	"io"
+	"archive/tar"
+	"strings"
 )
 
 // dockerAPI is an interface for stub testing.
@@ -26,7 +25,7 @@ type dockerAPI interface {
 
 // Client represents a Docker API client.
 type Client struct {
-	docker dockerAPI
+	docker *client.Client
 }
 
 // New creates the Client instance.
@@ -41,66 +40,106 @@ func New() (*Client, error) {
 	return &Client{docker: cli}, nil
 }
 
-// ExportImage exports a docker image into the archive of filesystem.
-// Save an environ of the docker image into `/.drootenv` to preserve it.
-func (c *Client) ExportImage(imageID string) (io.ReadCloser, error) {
-	ctx := context.Background()
-
-	image, _, err := c.docker.ImageInspectWithRaw(ctx, imageID)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to inspect image imageID:%s", imageID)
+func (c *Client) Inspect(ctx context.Context, id string) (info *types.ContainerJSON, containerId2Stop *string, err error) {
+	// check first existing container
+	inspect, err := c.docker.ContainerInspect(ctx, id)
+	if err == nil {
+		if inspect.State.Running {
+			return &inspect, nil, nil
+		}
+		if err := c.docker.ContainerStart(ctx, inspect.ID, types.ContainerStartOptions{}); err != nil {
+			_ = c.Remove(ctx, inspect.ID)
+			return nil, nil, errors.Wrapf(err, "Failed to start container %s", inspect.ID)
+		}
+		return &inspect, &inspect.ID, nil
 	}
-
-	//Put drootenv file into the filesystem.
-	cmd := fmt.Sprintf("echo \"%s\" > %s && echo \"%s\" > %s", strings.Join(image.ContainerConfig.Env, "\n"), environ.DROOT_ENV_FILE_PATH, strings.Join(image.Config.Entrypoint, " "), environ.DROOT_ENTRY_FILE_PATH)
 	container, err := c.docker.ContainerCreate(ctx, &container.Config{
-		Image:      imageID,
+		Image:      id,
 		User:       "root",       // Avoid permission denied error
-		Entrypoint: []string{""}, // Clear the exising entrypoint
-		Cmd:        []string{"/bin/sh", "-c", cmd},
 	}, nil, nil, "")
 	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to create container imageID:%s", imageID)
+		return nil, nil, errors.Wrapf(err, "Failed to create container from image %s", id)
 	}
+	return c.Inspect(ctx, container.ID)
+}
 
-	// start container because creating container does not run above `printenv > /.drootenv`.
-	if err := c.docker.ContainerStart(ctx, container.ID, types.ContainerStartOptions{}); err != nil {
-		_ = c.docker.ContainerRemove(ctx, container.ID, types.ContainerRemoveOptions{
-			Force: true,
-		})
-		return nil, errors.Wrapf(err, "Failed to remove container containerID:%s", container.ID)
+func (c *Client) Remove(ctx context.Context, containerID string) (error) {
+	if err := c.docker.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{
+		Force: true,
+	}); err != nil {
+		return errors.Wrapf(err, "Failed to remove container %s", containerID)
 	}
+	return nil
+}
 
-	code, err := c.docker.ContainerWait(ctx, container.ID)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Failed to wait container containerID:%s", container.ID)
+const DROOT_ENV_FILE_PATH = ".drootenv"
+const DROOT_ENTRY_FILE_PATH = ".drootentry.sh"
+
+func (c *Client) writeFakeFile(w *tar.Writer, path string, body string) (error) {
+	return c.write(
+		w,
+		&tar.Header{
+			Uname:    "root",
+			Gname:    "root",
+			Mode:     int64(0777),
+			Name:     path,
+			Typeflag: tar.TypeReg,
+			Size:     int64(len(body)),
+		},
+		[]byte(body),
+	)
+}
+
+func (c *Client) write(w *tar.Writer, h *tar.Header, b []byte) (error) {
+	if err := w.WriteHeader(h); err != nil {
+		return err
 	}
-	if code != int64(0) {
-		return nil, errors.Errorf("ContainerWait status code is not 0, but %d containerID:%s", code, container.ID)
+	if _, err := w.Write(b); err != nil {
+		return err
 	}
+	return nil
+}
 
-	pReader, pWriter := io.Pipe()
 
+// ExportImage exports a docker image into the archive of filesystem.
+// Save an environ of the docker image into `/.drootenv` to preserve it.
+func (c *Client) Export(ctx context.Context, containerID string, info *types.ContainerJSON) (io.ReadCloser, error) {
+	reader, writer := io.Pipe()
 	go func() {
-		defer func() {
-			_ = c.docker.ContainerRemove(ctx, container.ID, types.ContainerRemoveOptions{
-				Force: true,
-			})
-		}()
-
-		body, err := c.docker.ContainerExport(ctx, container.ID)
-		if err != nil {
-			err = errors.Wrapf(err, "Failed to export container containerID:%s", container.ID)
-			pWriter.CloseWithError(err)
-		} else {
-			_, err := io.Copy(pWriter, body)
-			if err != nil {
-				err = errors.Wrapf(err, "Failed to copy from reader of exporting container containerID:%s", container.ID)
-				pWriter.CloseWithError(err)
-			}
-			pWriter.Close()
+		w := tar.NewWriter(writer)
+		if err := c.writeFakeFile(w, DROOT_ENV_FILE_PATH, strings.Join(info.Config.Env, "\n")); err != nil {
+			writer.CloseWithError(errors.Wrapf(err, "Failed to write envs"))
+			return
 		}
+		if err := c.writeFakeFile(w, DROOT_ENTRY_FILE_PATH, strings.Join(append(info.Config.Entrypoint, info.Config.Cmd...), " ")); err != nil {
+			writer.CloseWithError(errors.Wrapf(err, "Failed to write entrypoint"))
+			return
+		}
+		body, err := c.docker.ContainerExport(ctx, containerID)
+		if err != nil {
+			writer.CloseWithError(errors.Wrapf(err, "Failed to export container %s", containerID))
+			return
+		}
+		r := tar.NewReader(body)
+		buffer := new(bytes.Buffer)
+		for {
+			h, err := r.Next()
+			if err  == io.EOF {
+				break
+			}
+			if err != nil {
+				writer.CloseWithError(errors.Wrapf(err, "Failed to read contents of container %s after export", containerID))
+				return
+			}
+			buffer.Reset()
+			buffer.ReadFrom(r)
+			if err := c.write(w, h, buffer.Bytes()); err != nil {
+				writer.CloseWithError(errors.Wrapf(err, "Failed to copy %s from container %s", h.Name, containerID))
+				return
+			}
+		}
+		w.Close()
+		writer.Close()
 	}()
-
-	return pReader, nil
+	return reader, nil
 }
